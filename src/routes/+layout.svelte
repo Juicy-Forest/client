a2bc1dc1dc2990c857676e4ecece1e3cb79f5129
kebabzter <gabzterthesecond@gmail.com>
2025-11-20 11:53:32 +0100

    Added temporary basic navigation and navLink components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import Navigation from '$lib/components/Navigation.svelte';
 
 	let { children } = $props();
 </script>
@@ -8,5 +9,7 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<Navigation/>
 
 {@render children()}
