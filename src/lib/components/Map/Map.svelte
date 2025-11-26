91b026eca7d6d827ca427b56a2a7c915b74b31c8
alexmanev-code <mane0003@hz.nl>
2025-11-26 21:48:57 +0100

    inital app layout configuration/map setup initialized

---
+++ src/lib/components/Map/Map.svelte
@@ -0,0 +1,13 @@
+<div class="rectangle">
+
+</div>
+
+
+<style>
+  .rectangle {
+    width: 200px;
+    height: 100px;
+    background-color: lightblue;
+    border: 2px solid black;
+  }
+</style>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,4 +1,18 @@
 <script>
-    import Construction from "$lib/components/Construction.svelte";
+    import Map from '$lib/components/Map/Map.svelte';
 </script>
-<Construction text={"Map"}/>
+<div class="map-container">
+    <Map/>
+</div>
+
+<style>
+.map-container {
+        display: flex;
+        justify-content: center; 
+        align-items: center;     
+        width: 100%;             
+        min-height: calc(100vh - 6rem); 
+        padding-top: 3rem;     
+        box-sizing: border-box;
+    }
+</style>
